3fe4b131bb7e386435ebc0933bde779f25ddfb4a
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-01-14 21:44:01 +0000

    Pattern matching, lists and loops

--- Program.fs
+++ Program.fs
@@ -1,13 +1,32 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
 
-let greet(name:string) =
+let greet(name: string) =
     printfn $"Yo, {name}!"
 
-let rec greetEcho(name:string, echoes:int) =
+let rec greetEcho(name: string, echoes: int) =
     printfn $"Hello, {name}"
     if echoes > 1 then greetEcho (name, echoes-1)
 
-greet("Ryan")
+greet"Ryan"
 
 greetEcho("Ryan", 3)
+
+type Pets =
+    | Dog of sound: string
+    | Cat of sound: string
+    | Parrot of sound: string
+
+let petSound pet =
+    match pet with
+    | Dog sound -> $"{sound} {sound} {sound}"
+    | Cat sound -> $"{sound} {sound} {sound}"
+    | Parrot sound -> $"{sound} {sound} {sound}"
+
+let printPets pets =
+    for pet in pets do
+        printfn $"The pet goes {petSound pet}"
+
+let myPets = [Dog "woof"; Cat "meow"; Parrot "chirp"]
+
+printPets myPets
