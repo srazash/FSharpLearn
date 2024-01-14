3b2d7ece2f680e0690afeb9531d05d849f8ecda2
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-01-14 19:57:27 +0000

    Initial commit: defining functions and recursive functions

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let greet(name:string) =
+    printfn $"Yo, {name}!"
+
+let rec greetEcho(name:string, echoes:int) =
+    printfn $"Hello, {name}"
+    if echoes > 1 then greetEcho (name, echoes-1)
+
+greet("Ryan")
+
+greetEcho("Ryan", 3)
